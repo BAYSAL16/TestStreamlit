55e764c30418a88e2b3bb918801ac983d7500ec2
Emir BAYSAL <baysalemir64@gmail.com>
2026-06-05 10:38:56 +0300

    FirstAPP

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st #Streamlit
+# pip install streamlit
+
+st.title("Merhaba")
+
+if st.button("Tikla"):
+    st.write("Merhaba , Stramlit")
+else:
+    st.write("BUTONA BAS")
+
+#Veri alma
+kilo = st.number_input("Kilo (kg)",min_value=0.0)
+yas = st.number_input("Boy (cm)",min_value=0.0)
+
+if st.button("Hesapla"):
+    if yas > 0 :
+        vk = kilo / (yas/100)
+        st.metric("Sonuc:",f"{vk}")
+    else:
+        st.error("HATA")
+        
